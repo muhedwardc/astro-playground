bccd16e2ef3defdaf7cf179d5f9e080155e30dd9
muhedwardc <muhedwardc@gmail.com>
2023-02-01 21:15:26 +0700

    add netlify edge adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
+import netlify from "@astrojs/netlify/edge-functions";
 
 // https://astro.build/config
 import solidJs from "@astrojs/solid-js";
@@ -8,5 +9,6 @@ import solidJs from "@astrojs/solid-js";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
+  adapter: netlify(),
   integrations: [vue(), tailwind(), solidJs()],
 });
