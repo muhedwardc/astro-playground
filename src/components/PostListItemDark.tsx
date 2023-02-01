4ae9d963fae521e45ec94391bda4f2cda9d9e4f2
muhedwardc <muhedwardc@gmail.com>
2023-02-01 21:57:53 +0700

    inline list item classes

--- src/components/PostListItemDark.tsx
+++ src/components/PostListItemDark.tsx
@@ -8,24 +8,7 @@ interface Post {
 
 const PostListItemDark = ({ id, title, body, sub }: Post) => {
     return (
-        <li class="
-            mt-6
-            w-full
-            list-none
-            p-[0.15rem]
-            flex
-            bg-purple-800
-            rounded-[0.5rem]
-            text-white
-            shadow-lg
-            border-2
-            hover:border-accent
-            hover:bg-gradient-to-r
-            hover:from-purple-800
-            hover:to-purple-400
-            transition-all
-        "
-        >
+        <li class="mt-6 w-full list-none p-[0.15rem] flex bg-purple-800 rounded-[0.5rem] text-white shadow-lg border-2 hover:border-accent hover:bg-gradient-to-r hover:from-purple-800 hover:to-purple-400 transition-all">
             <a href={`/post/${id}`} class="no-underline leading-[1.4] px-[1.3rem] py-[1rem] opacity-80">
                 <h2 class="m-0 text-md">{ title }</h2>
                 <p class="mt-2 mb-0 text-slate-200">{ body }</p>
