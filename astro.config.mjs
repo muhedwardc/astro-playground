5e80fcd31573c1c2a153cc996b81ad14d442bf5c
muhedwardc <muhedwardc@gmail.com>
2023-02-06 21:10:44 +0700

    add site name

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,11 @@ import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
 import netlify from "@astrojs/netlify/edge-functions";
-
-// https://astro.build/config
 import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://ed-astro-playground.netlify.app",
   output: "server",
   adapter: netlify(),
   integrations: [vue(), tailwind(), solidJs()],
