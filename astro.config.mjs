63e381ebc317afeee575aa14faee493f2019b0a2
muhedwardc <muhedwardc@gmail.com>
2023-02-06 21:29:09 +0700

    update adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from "astro/config";
 import vue from "@astrojs/vue";
 import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify/edge-functions";
 import solidJs from "@astrojs/solid-js";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://ed-astro-playground.netlify.app",
   output: "server",
-  adapter: netlify(),
+  adapter: vercel(),
   integrations: [vue(), tailwind(), solidJs()],
 });
