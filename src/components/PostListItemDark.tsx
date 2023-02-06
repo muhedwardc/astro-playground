ddedb17d59bd0a0a64d493f1f9beaa6b4fe20029
muhedwardc <muhedwardc@gmail.com>
2023-02-06 18:17:03 +0700

    add confirmation alert on solid post list item click event

--- src/components/PostListItemDark.tsx
+++ src/components/PostListItemDark.tsx
@@ -5,11 +5,17 @@ interface Post {
     sub: string;
 }
 
+const showConfirmation = (id: number, title: string) => {
+    if (confirm(`Open "${title}"?`) === true) {
+        window.open(`/post/${id}`, '_self')
+    }
+}
+
 
 const PostListItemDark = ({ id, title, body, sub }: Post) => {
     return (
-        <li class="mt-6 w-full list-none p-[0.15rem] flex bg-purple-800 rounded-[0.5rem] text-white shadow-lg border-2 hover:border-accent hover:bg-gradient-to-r hover:from-purple-800 hover:to-purple-400 transition-all">
-            <a href={`/post/${id}`} class="no-underline leading-[1.4] px-[1.3rem] py-[1rem] opacity-80">
+        <li class="mt-6 w-full list-none p-[0.15rem] flex bg-purple-800 rounded-[0.5rem] text-white shadow-lg border-2 hover:border-accent hover:bg-gradient-to-r hover:from-purple-800 hover:to-purple-400 transition-all cursor-pointer">
+            <a onClick={() => showConfirmation(id, title)} class="no-underline leading-[1.4] px-[1.3rem] py-[1rem] opacity-80">
                 <h2 class="m-0 text-md">{ title }</h2>
                 <p class="mt-2 mb-0 text-slate-200">{ body }</p>
                 <p class="mt-3 mb-0 text-slate-100 float-right italic">{ sub }</p>
